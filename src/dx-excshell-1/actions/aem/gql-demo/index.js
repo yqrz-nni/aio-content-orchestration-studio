84a6a0a5a28c224aede9effe7d41fa357381c89c
Mark Raby <mraby@scholastic.com>
2026-02-04 21:50:18 -0500

    first steps

--- src/dx-excshell-1/actions/aem/gql-demo/index.js
+++ src/dx-excshell-1/actions/aem/gql-demo/index.js
@@ -1,4 +1,5 @@
 const fetch = require("node-fetch");
+const auth = require("@adobe/jwt-auth");
 
 function json(statusCode, body) {
   return {
@@ -10,6 +11,17 @@ function json(statusCode, body) {
 
 async function main(params) {
     try {
+        // 1) Exchange JWT creds for IMS access token
+        const accessTokenResp = await auth({
+            imsHost: params.IMS_HOST,                 // e.g. ims-na1.adobelogin.com
+            clientId: params.CLIENT_ID,
+            clientSecret: params.CLIENT_SECRET,
+            technicalAccountId: params.TECH_ACCOUNT_ID, // your "id" (…@techacct.adobe.com)
+            orgId: params.ORG_ID,                     // …@AdobeOrg
+            privateKey: (params.PRIVATE_KEY || "").replace(/\\r\\n/g, "\n"),
+            metaScopes: { [params.METASCOPES]: true } // e.g. { ent_aem_cloud_api: true }
+        });
+        const accessToken = accessTokenResp.access_token || accessTokenResp;
         return json(200, { message: "AEM GraphQL Demo Action is working!" });
     } catch (error) {
       return json(500, { error: error.message });
