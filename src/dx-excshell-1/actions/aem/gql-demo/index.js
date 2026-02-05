e0281e9a52db2367ce1e348a0faf0599c74f8bef
Mark Raby <mraby@scholastic.com>
2026-02-04 23:44:40 -0500

    cleanup

--- src/dx-excshell-1/actions/aem/gql-demo/index.js
+++ src/dx-excshell-1/actions/aem/gql-demo/index.js
@@ -10,51 +10,58 @@ function json(statusCode, body) {
 }
 
 async function main(params) {
-    try {
-        // 1) Exchange JWT creds for IMS access token
-        const accessTokenResp = await auth({
-            imsHost: params.IMS_HOST,                 // e.g. ims-na1.adobelogin.com
-            clientId: params.CLIENT_ID,
-            clientSecret: params.CLIENT_SECRET,
-            technicalAccountId: params.TECH_ACCOUNT_ID, // your "id" (…@techacct.adobe.com)
-            orgId: params.ORG_ID,                     // …@AdobeOrg
-            privateKey: (params.PRIVATE_KEY || "").replace(/\\r\\n/g, "\n"),
-            metaScopes: params.METASCOPES
-        });
-        const accessToken = accessTokenResp.access_token || accessTokenResp;
-        // 2) Call AEM GraphQL (Author)
-        // const gqlUrl = `${params.AEM_AUTHOR}${params.AEM_GQL_PATH}`;
-        const gqlProxyUrl = `${params.AEM_GQL_PATH_PROXY}`;
-        gqlUrl = gqlProxyUrl;
-        const query = `
-            query {
-                unifiedPromotionalContentList(limit: 5) {
-                    items {
-                        _path
-                        _id
-                        headlineText
-                    }
-                }
-            }
-        `;
-        const r = await fetch(gqlUrl, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            Authorization: `Bearer ${accessToken}`,
-                "x-gw-ims-org-id": params.ORG_ID,
-                "x-api-key": params.CLIENT_ID
-            },
-            body: JSON.stringify({ query })
-        });
-        const text = await r.text();
-        if (!r.ok) {
-            return { statusCode: r.status, body: { error: text } };
-        }
-        return json(200, { message: JSON.parse(text) });
-    } catch (error) {
-      return json(500, { error: error.message });
+  try {
+    const useProxy = params.USE_AEM_PROXY === "true";
+
+    const gqlUrl = useProxy
+      ? params.AEM_GQL_PATH_PROXY
+      : new URL(params.AEM_GQL_PATH, params.AEM_AUTHOR).toString();
+
+    let headers = {
+      "Content-Type": "application/json",
+    };
+
+    // Only mint token if NOT using proxy
+    if (!useProxy) {
+      const accessTokenResp = await auth({
+        imsHost: params.IMS_HOST,
+        clientId: params.CLIENT_ID,
+        clientSecret: params.CLIENT_SECRET,
+        technicalAccountId: params.TECH_ACCOUNT_ID,
+        orgId: params.ORG_ID,
+        privateKey: (params.PRIVATE_KEY || "").replace(/\\r\\n/g, "\n"),
+        metaScopes: params.METASCOPES,
+      });
+
+      const accessToken =
+        accessTokenResp.access_token || accessTokenResp;
+
+      headers.Authorization = `Bearer ${accessToken}`;
+      headers["x-gw-ims-org-id"] = params.ORG_ID;
+      headers["x-api-key"] = params.CLIENT_ID;
     }
-};
+
+    const query = `
+      query {
+        unifiedPromotionalContentList(limit: 5) {
+          items { _path _id headlineText }
+        }
+      }
+    `;
+
+    const r = await fetch(gqlUrl, {
+      method: "POST",
+      headers,
+      body: JSON.stringify({ query }),
+    });
+
+    const text = await r.text();
+    if (!r.ok) return json(r.status, { error: text });
+
+    return json(200, JSON.parse(text));
+  } catch (error) {
+    return json(500, { error: error.message });
+  }
+}
 
 exports.main = main;
